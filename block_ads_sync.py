53f88f5985f8dd54b31d50e018e2cff3244b1f64
brojangles24 <68071853+brojangles24@users.noreply.github.com>
2025-12-04 14:14:32 -0700

    Update block_ads_sync.py

--- block_ads_sync.py
+++ block_ads_sync.py
@@ -50,7 +50,7 @@ class Config:
         #"https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt", #Aundeep Servers ~42k domains
         #"https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/light-onlydomains.txt", #Hagezi Light ~ 40k domains 
         #"https://adaway.org/hosts.txt", # Adaway Hosts ~6k domains
-        "https://raw.githubusercontent.com/brojangles24/Cloudflare-ZeroTrust-AdBlock/refs/heads/main/HaGeZi_Light.txt", 
+        "https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt",
     ]
 
     @classmethod
