1620ae1aced4c0d7d967469cfeca7d474633e779
brojangles24 <68071853+brojangles24@users.noreply.github.com>
2026-01-11 17:31:07 -0700

    Comment out 'Fake Hosters' URL
    Comment out the 'Fake Hosters' URL in the block_ads_sync.py file.

--- block_ads_sync.py
+++ block_ads_sync.py
@@ -35,7 +35,7 @@ MASTER_CONFIG = {
         "HaGeZi Pro++": "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/pro.plus-onlydomains.txt",
         #"1Hosts Lite": "https://raw.githubusercontent.com/badmojr/1Hosts/refs/heads/master/Lite/domains.wildcards",
         "TIF Mini": "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/tif.mini-onlydomains.txt",
-        "Fake Hosters": "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/fake-onlydomains.txt",
+        #"Fake Hosters": "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/fake-onlydomains.txt",
     }
 }
 
