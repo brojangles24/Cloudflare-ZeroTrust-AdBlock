1cdad5d01751e7cb49b06578a585527faa1321ef
brojangles24 <68071853+brojangles24@users.noreply.github.com>
2026-06-07 09:43:20 -0700

    Update block_ads_sync.py

--- block_ads_sync.py
+++ block_ads_sync.py
@@ -75,7 +75,7 @@ BLOCKLIST_URLS = {
         "https://raw.githubusercontent.com/sjhgvr/oisd/refs/heads/main/abp_nsfw.txt",
         "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn-only/hosts",
         "https://raw.githubusercontent.com/blocklistproject/Lists/master/porn.txt",
-        "https://raw.githubusercontent.com/Bon-Appetit/porn-domains/main/block.txt",
+        "https://raw.githubusercontent.com/Bon-Appetit/porn-domains/refs/heads/main/block.5765c77bab.xpsq3l.txt",
     ],
     "HaGeZi Fake": "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/fake-onlydomains.txt",
     "HaGeZi TIF Full": "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/tif-onlydomains.txt",
