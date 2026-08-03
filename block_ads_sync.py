bdd1f31730663a74fc6799e78f59abf7816bf7c2
brojangles24 <68071853+brojangles24@users.noreply.github.com>
2026-08-03 11:40:53 -0700

    Update block_ads_sync.py

--- block_ads_sync.py
+++ block_ads_sync.py
@@ -270,7 +270,7 @@ def fetch_url(session: requests.Session, name: str, url: str | list[str], checke
             resp = session.get(target_url, timeout=Config.REQUEST_TIMEOUT)
             resp.raise_for_status()
             
-            skip_relevance = False
+            skip_relevance = False #(name == "HaGeZi Normal")
 
             for line in resp.text.splitlines():
                 line = line.strip()
