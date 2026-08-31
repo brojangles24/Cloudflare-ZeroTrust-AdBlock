97492c047bb59fa3c7208d33aad46b5bcaa29931
brojangles24 <68071853+brojangles24@users.noreply.github.com>
2026-08-31 14:00:08 -0700

    Update block_ads_sync.py

--- block_ads_sync.py
+++ block_ads_sync.py
@@ -92,7 +92,7 @@ BLOCKLIST_SOURCES = [
     {
         "name": "HaGeZi TIF Full",
         "url": ["https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/tif.mini-onlydomains.txt"],
-        "enable_relevance": True
+        "enable_relevance": False
     },
     {
         "name": "HaGeZi Social",
