7e2d08dedcda497fbd29805b6ac49417fd1f9d2c
brojangles24 <68071853+brojangles24@users.noreply.github.com>
2026-09-02 08:04:42 -0700

    Update block_ads_sync.py

--- block_ads_sync.py
+++ block_ads_sync.py
@@ -63,7 +63,7 @@ BLOCKLIST_SOURCES = [
    #},
     {
         "name": "HaGeZi Pro",
-        "url": "https://raw.githubusercontent.com/luckmagnet/1Hosts/refs/heads/master/Lite/domains.wildcards",
+        "url": "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/pro-onlydomains.txt",
         "enable_relevance": True
     },
     {
