5b75209c5239cd2d5ab1dbfc40fa9e9285427e2f
brojangles24 <68071853+brojangles24@users.noreply.github.com>
2026-09-02 14:54:15 -0700

    Update block_ads_sync.py

--- block_ads_sync.py
+++ block_ads_sync.py
@@ -63,7 +63,7 @@ BLOCKLIST_SOURCES = [
    },
    {
         "name": "HaGeZi Pro",
-        "url": "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/pro.plus-onlydomains.txt",
+        "url": "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/pro-onlydomains.txt",
         "enable_relevance": True
     },
     {
