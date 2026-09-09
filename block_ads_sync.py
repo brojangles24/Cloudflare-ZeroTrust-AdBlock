0b723b99f7790a822d2afeca422eed9437962eaa
brojangles24 <68071853+brojangles24@users.noreply.github.com>
2026-09-09 16:09:01 -0700

    Update block_ads_sync.py

--- block_ads_sync.py
+++ block_ads_sync.py
@@ -81,11 +81,11 @@ BLOCKLIST_SOURCES = [
     #    "url": "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/popupads-onlydomains.txt",
     #    "enable_relevance": True
     #},
-    {
-        "name": "HaGeZi Badware",
-        "url": "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/hoster-onlydomains.txt",
-        "enable_relevance": True
-    },
+    #{
+    #    "name": "HaGeZi Badware",
+    #    "url": "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/hoster-onlydomains.txt",
+    #    "enable_relevance": True
+    #},
     {
         "name": "HaGeZi Fake",
         "url": "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/fake-onlydomains.txt",
@@ -164,7 +164,7 @@ def get_active_policies():
                 "HaGeZi Normal",
                 "Hagezi NSFW", 
                 #"HaGeZi Popups",
-                "HaGeZi Badware",
+                #"HaGeZi Badware",
                 "HaGeZi Fake", 
                 "HaGeZi No SafeSearch", 
                 "HaGeZi TIF Full",
@@ -173,7 +173,7 @@ def get_active_policies():
                 "HaGeZi Anti Piracy", 
             ], 
             "exclude": [],
-            "use_spam_tld": True
+            "use_spam_tld": False
         },
         {
             "prefix": "L_Restrictive", 
